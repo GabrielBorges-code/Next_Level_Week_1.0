940273b8e0f68c6ed941f3ac1e55b6eb0b13fc01
Gabriel <gabriel.moura2009@hotmial.com>
2020-06-04 20:03:50 -0300

    criando rotas

--- NLW/src/server.js
+++ NLW/src/server.js
@@ -1,12 +1,23 @@
 const express = require("express")
 const server = express()
 
-//setting my application steps
+//setting public page
+server.use(express.static("public"))
 
+//setting my application steps
 //initial page
 //req = request res = answer
 server.get("/", (req, res) =>{
-    res.send("Hello world!")
+    res.sendFile(__dirname + "/views/index.html")
+})
+server.get("/create-point", (req, res) =>{
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+server.get("/create-point", (req, res) =>{
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+server.get("/search-results", (req, res) =>{
+    res.sendFile(__dirname + "/views/search-results.html")
 })
 //on server
 server.listen(3000)
