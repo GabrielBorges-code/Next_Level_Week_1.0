ef299c0f6b569832055fbfa6fed89c8b5354d20e
Gabriel <gabriel.moura2009@hotmial.com>
2020-06-03 21:52:51 -0300

    arrumando o javascript

--- NLW/scripts/create-point.js
+++ NLW/scripts/create-point.js
@@ -46,3 +46,46 @@ document
     .querySelector("select[name=uf]")
     //() => função anonima
     .addEventListener("change", getCities)
+
+//Items de coleta
+//Pegar todos os li
+
+const ItemsToCollect = document.querySelectorAll(".items-grid li")
+for (const item of ItemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const colletedItems =  document.querySelector("input[name=items")
+
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemL1 = event.target
+
+    // add ou remover uma class
+    itemL1.classList.toggle("selected")
+
+    const itemId = itemL1.dataset.id
+    
+    //verificar se tem items selecionados e pegar eles
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId//retorna true ou false
+        return itemFound
+    })
+
+    //se já estiver selecionado retirar da seleção
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    }else{
+        //se não estiver adicionar a seleção
+        //adicionar elemento
+        selectedItems.push(itemId)
+    }    
+    //atualizar o campo escondido com items selecionados
+    colletedItems.value = selectedItems
+}
