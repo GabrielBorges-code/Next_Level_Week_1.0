669b7a87dcefc0f898e07f6863b43b111763c239
Gabriel <gabriel.moura2009@hotmial.com>
2020-06-05 23:17:06 -0300

    organizando_páginas

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express()
+
+/*//setting public page
+server.use(express.static("public"))
+
+//using tamplate engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views",{
+    express: server,
+    noCache: true
+})
+
+//setting my application steps
+//initial page
+//req = request res = answer
+server.get("/", (req, res) =>{
+    return res.render("index.html")
+})
+server.get("/create-point", (req, res) =>{
+    return res.render("create-point.html")
+})
+server.get("/search", (req, res) =>{
+    return res.render("search-results.html")
+})*/
+
+//on server
+server.listen(3000)
