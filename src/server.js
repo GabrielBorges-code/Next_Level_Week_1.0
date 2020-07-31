936b7559347f7b9c1442e2b6232fd98e1aa6dbee
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-31 03:42:10 -0300

    pages ok server ok

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
-const express = require ("express")
-const server = express ()
+const express = require ("express");
+const server = express ();
+
+//configure public folder
+server.use(express.static("public"));
+
+//configure my page paths
+
+//pages
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html");
+});
 
 //on server
-server.listen(3000)
+server.listen(3000);
